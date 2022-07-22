cfa905e71d81f67d738967c838be1826b6e6ef12
Noah Loomis <nloomis@spiria.com>
2022-07-22 11:19:14 -0400

    Adde

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,10 @@
 import { useState } from "react";
 import { Tabs, Tab, Box, Container } from "@mui/material";
+import { motion, AnimatePresence } from "framer-motion";
 
 import About from "./About";
 import Projects from "./Projects";
 import Home from "./Home";
-
 import TabPanel from "./TabPanel";
 import a11yProps from "../assets/a11y";
 
@@ -25,15 +25,25 @@ const App = () => {
         </Tabs>
       </Box>
       <Container maxWidth="xl">
-        <TabPanel value={selected} index={0}>
-          <Home />
-        </TabPanel>
-        <TabPanel value={selected} index={1}>
-          <About />
-        </TabPanel>
-        <TabPanel value={selected} index={2}>
-          <Projects />
-        </TabPanel>
+        <AnimatePresence exitBeforeEnter>
+          <motion.div
+            key={selected ? selected : 0}
+            initial={{ y: 10, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            exit={{ y: -10, opacity: 0 }}
+            transition={{ duration: 0.2 }}
+          >
+            <TabPanel value={selected} index={0}>
+              <Home />
+            </TabPanel>
+            <TabPanel value={selected} index={1}>
+              <About />
+            </TabPanel>
+            <TabPanel value={selected} index={2}>
+              <Projects />
+            </TabPanel>
+          </motion.div>
+        </AnimatePresence>
       </Container>
     </>
   );
--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,9 +1,55 @@
-const Home = () => {
-    return (
-        <div>
-            Home
-        </div>
-    )
-}
+import { motion, Variants } from "framer-motion";
 
-export default Home
+const Home = () => {
+  const homeVarients: Variants = {
+    offscreen: {
+      y: 500,
+    },
+    onscreen: {
+      y: 0,
+      paddingTop: '10%',
+      transition: {
+        type: "spring",
+        bounce: 0.4,
+        duration: 0.8,
+      },
+    },
+  };
+  return (
+    <div>
+      <motion.div
+        className="card-container"
+        initial="onscreen"
+        whileInView="onscreen"
+        viewport={{ once: true, amount: 0.8 }}
+      >
+        <motion.div className="card" variants={homeVarients}>
+          Personal section
+        </motion.div>
+      </motion.div>
+      <motion.div
+        className="card-container"
+        initial="offscreen"
+        whileInView="onscreen"
+        viewport={{ once: true, amount: 0.8 }}
+      >
+        <motion.div className="card" variants={homeVarients} style={{backgroundColor: 'red'}}>
+          Personal section
+        </motion.div>
+      </motion.div>
+
+      <motion.div
+        className="card-container"
+        initial="offscreen"
+        whileInView="onscreen"
+        viewport={{ once: true, amount: 0.8 }}
+      >
+        <motion.div className="card" variants={homeVarients}>
+          Personal section
+        </motion.div>
+      </motion.div>
+    </div>
+  );
+};
+
+export default Home;
